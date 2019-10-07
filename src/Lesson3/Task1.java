c5211c380ef2f8348ffd09ea81631fe1dc64653b
Andrii <andriy.servatiak@gmail.com>
2019-10-07 16:00:25 +0300

    Done

--- src/Lesson3/Task1.java
+++ src/Lesson3/Task1.java
@@ -1,12 +1,10 @@
 package Lesson3;
 
-import java.util.Random;
-
 public class Task1 {
     public static void main(String[] args) {
         var c = "-";
-        var n="*";
-        var q="+";
+        var n = "*";
+        var q = "+";
         var a = 0;
         var b = 0;
         if (c == "-") {
@@ -16,11 +14,11 @@ public class Task1 {
             a = 5;
             b = 8;
 
-        } else if (q=="+"){
+        } else if (q == "+") {
             a = 1;
             b = 9;
 
-        }else {
+        } else {
             System.out.println("Eror");
         }
         System.out.println(a + 11 / 12 * (b + b) / 12);
--- src/Lesson3/Task2.java
+++ src/Lesson3/Task2.java
@@ -2,11 +2,22 @@ package Lesson3;
 
 public class Task2 {
     public static void main(String[] args) {
-        var i = 0;
-        while (i <= 99) {
-            i++;
-            System.out.println(i);
+        int i = 0;
+        int b = 0;
+        String k = "";
+
+        for (i = 1; i <= 100; i++) {
+            int d = 0;
+            for (b = i; b >= 1; b--) {
+                if (i % b == 0) {
+                    d = d + 1;
+                }
+            }
+            if (d == 2) {
+                k = k + i + " ";
+            }
         }
+        System.out.println(k);
 
     }
 
