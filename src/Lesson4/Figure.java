dc124bda6bd81c9a6653f199f1f4e4a3a3892ec1
Andrii <andriy.servatiak@gmail.com>
2019-10-09 23:31:40 +0300

    Done

---
+++ src/Lesson4/Figure.java
@@ -0,0 +1,18 @@
+package Lesson4;
+
+public class Figure {
+
+    public String name;
+    public int size;
+    public Figure(String name,int size){
+        this.name=name;
+        this.size=size;
+    }
+    public void area(){
+
+    }
+    public void perimetr(){
+
+    }
+
+}
---
+++ src/Lesson4/TestArray.java
@@ -0,0 +1,15 @@
+package Lesson4;
+
+public class TestArray {
+    public static void main(String[] args) {
+        var array=new Array(5);
+        array.fillArray();
+        array.swapPairElements(11);
+        array.printArray();
+        var figure=new Figure("Cirle",23);
+
+
+
+    }
+
+}
