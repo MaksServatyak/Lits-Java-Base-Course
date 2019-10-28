7c38c7032f45503331b93c1454ae7979a6e103c7
Andrii <andriy.servatiak@gmail.com>
2019-10-28 22:46:24 +0200

    Homework9

---
+++ src/Lesson4/Homework9/RegularForEmail.java
@@ -0,0 +1,20 @@
+package Lesson4.Homework9;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularForEmail {
+    public static void main(String[] args) {
+        String line="maksservatya@gmail.com";
+        String pattern="([w.-]+)+(@)([w-]+)(.[a-zA-z]+)";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(line);
+        if (m.matches()){
+            System.out.println("Good");
+        }else {
+            System.out.println("NO MATCH");
+        }
+
+
+    }
+}
