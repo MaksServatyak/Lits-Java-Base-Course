7dd5d5ba4c1e9835c9dd2e242100fa9d2dae6582
Andrii <andriy.servatiak@gmail.com>
2019-11-07 10:20:19 +0200

    Homework12

---
+++ src/Lesson12/Homewrok12/NameOfDrinks.java
@@ -0,0 +1,9 @@
+package Lesson12.Homewrok12;
+
+public class NameOfDrinks<T extends NameOfDrinks> {
+
+    T pepsi;
+    T cola;
+
+
+}
---
+++ src/Lesson12/Homewrok12/StackOverflow.java
@@ -0,0 +1,8 @@
+package Lesson12.Homewrok12;
+
+public class StackOverflow {
+    public void recursionFunction() {
+        System.out.println(858);
+        recursionFunction();
+    }
+}
